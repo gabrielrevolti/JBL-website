042046d2e46944af21717a6c87d738d6bf1f7d28
Gabriel Padilha <gabriel.revolti@gmail.com>
2023-10-06 19:43:26 -0300

    project finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,15 +31,72 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== SWIPER FAVORITES ===============*/ 
+let swiperFavorite = new Swiper('.favorite__swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+    grabCursor: true,
 
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        }
+    }
+})
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true, //animations repeat
+
+})
+
+sr.reveal(`.home__social, .favorite__container, .sponsor__container, .footer`)
+sr.reveal(`.home__title span:nth-child(1)`, {origin: 'left', opacity: 1})
+sr.reveal(`.home__title span:nth-child(3)`, {origin: 'right', opacity: 1})
+sr.reveal(`.home__tooltip, .home__button, .model__button`, {origin: 'bottom'})
+sr.reveal(`.about__data`, {origin: 'left'})
+sr.reveal(`.about__img, .model__tooltip`, {origin: 'right'})
+
